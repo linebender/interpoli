128049013fa6439ec53a9565b136e02e43247626
Bruce Mitchener <bruce.mitchener@gmail.com>
2024-08-17 09:24:48 +0700

    Initial code from Velato (#2)
    Initial code from Velato
    
    This is the initial code from Velato's `src/runtime/model` but with some
    aspects removed, mainly the `Layer` concept: `Content`, `Draw`,
    `GroupTransform`, `Layer`, `Mask`, `Matte`, `Shape`.
    
    Minimal changes have been made to get it to compile and be free of lint
    warnings by disabling the lints.
    
    Subsequent changes will address lints and perform further cleanups and
    changes to prepare for an initial release.

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,105 @@
 #![warn(unused_crate_dependencies)]
 
 //! # Interpoli
+
+extern crate alloc;
+
+use alloc::vec::Vec;
+use kurbo::{Affine, PathEl, Point, Shape as _, Size, Vec2};
+use peniko::Color;
+
+mod spline;
+mod value;
+
+pub mod animated;
+pub mod fixed;
+
+pub use value::{Animated, Easing, EasingHandle, Time, Tween, Value, ValueRef};
+
+pub(crate) use spline::SplineToPath;
+
+macro_rules! simple_value {
+    ($name:ident) => {
+        #[allow(clippy::large_enum_variant)]
+        #[derive(Clone, Debug)]
+        pub enum $name {
+            Fixed(fixed::$name),
+            Animated(animated::$name),
+        }
+
+        impl $name {
+            pub fn is_fixed(&self) -> bool {
+                matches!(self, Self::Fixed(_))
+            }
+            pub fn evaluate(&self, frame: f64) -> ValueRef<fixed::$name> {
+                match self {
+                    Self::Fixed(value) => ValueRef::Borrowed(value),
+                    Self::Animated(value) => ValueRef::Owned(value.evaluate(frame)),
+                }
+            }
+        }
+    };
+}
+
+simple_value!(Transform);
+simple_value!(Stroke);
+simple_value!(Repeater);
+simple_value!(ColorStops);
+
+#[derive(Clone, Debug)]
+pub enum Brush {
+    Fixed(fixed::Brush),
+    Animated(animated::Brush),
+}
+
+impl Brush {
+    pub fn is_fixed(&self) -> bool {
+        matches!(self, Self::Fixed(_))
+    }
+
+    pub fn evaluate(&self, alpha: f64, frame: f64) -> ValueRef<fixed::Brush> {
+        match self {
+            Self::Fixed(value) => {
+                if alpha == 1.0 {
+                    ValueRef::Borrowed(value)
+                } else {
+                    ValueRef::Owned(fixed::brush_with_alpha(value, alpha))
+                }
+            }
+            Self::Animated(value) => ValueRef::Owned(value.evaluate(alpha, frame)),
+        }
+    }
+}
+
+impl Default for Transform {
+    fn default() -> Self {
+        Self::Fixed(Affine::IDENTITY)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum Geometry {
+    Fixed(Vec<PathEl>),
+    Rect(animated::Rect),
+    Ellipse(animated::Ellipse),
+    Spline(animated::Spline),
+}
+
+impl Geometry {
+    pub fn evaluate(&self, frame: f64, path: &mut Vec<PathEl>) {
+        match self {
+            Self::Fixed(value) => {
+                path.extend_from_slice(value);
+            }
+            Self::Rect(value) => {
+                path.extend(value.evaluate(frame).path_elements(0.1));
+            }
+            Self::Ellipse(value) => {
+                path.extend(value.evaluate(frame).path_elements(0.1));
+            }
+            Self::Spline(value) => {
+                value.evaluate(frame, path);
+            }
+        }
+    }
+}
