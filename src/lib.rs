3e17dd0275d77c2d9898ad109cac2ef7680ed114
Kaur Kuut <strom@nevermore.ee>
2024-11-23 13:31:10 +0200

    Update to Linebender lint set v2. (#33)
    Solved
    [`cargo_common_metadata`](https://rust-lang.github.io/rust-clippy/master/index.html#cargo_common_metadata),
    deferred others.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,28 @@
 // Copyright 2024 the Interpoli Authors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-#![cfg_attr(all(not(feature = "std"), not(test)), no_std)]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
-#![warn(unused_crate_dependencies)]
-
 //! # Interpoli
+
+// LINEBENDER LINT SET - lib.rs - v1
+// See https://linebender.org/wiki/canonical-lints/
+// These lints aren't included in Cargo.toml because they
+// shouldn't apply to examples and tests
+#![warn(unused_crate_dependencies)]
+#![warn(clippy::print_stdout, clippy::print_stderr)]
+// END LINEBENDER LINT SET
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(all(not(feature = "std"), not(test)), no_std)]
+// The following lints are part of the Linebender standard set,
+// but resolving them has been deferred for now.
+// Feel free to send a PR that solves one or more of these.
+#![allow(
+    missing_docs,
+    single_use_lifetimes,
+    elided_lifetimes_in_paths,
+    clippy::use_self,
+    clippy::cast_possible_truncation,
+    clippy::exhaustive_enums
+)]
 
 extern crate alloc;
 
