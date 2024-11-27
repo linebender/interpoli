ed044307b0a5a99b6b7c60cb762f6c51a8981642
Kaur Kuut <strom@nevermore.ee>
2024-11-27 12:01:26 +0200

    Add a dummy test to satisfy `nextest`. (#34)
    Keeps the CI functioning with test running assurances.

--- src/lib.rs
+++ src/lib.rs
@@ -104,3 +104,11 @@ impl Default for Transform {
         Self::Fixed(Affine::IDENTITY)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    // CI will fail unless cargo nextest can execute at least one test per workspace.
+    // Delete this dummy test once we have an actual real test.
+    #[test]
+    fn dummy_test_until_we_have_a_real_test() {}
+}
