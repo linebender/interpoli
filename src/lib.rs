34782deff85c0bd9ff6e572ce68ba0e62fa65806
Kaur Kuut <strom@nevermore.ee>
2024-11-27 11:47:38 +0200

    Add a dummy test to satisfy `nextest`.

--- src/lib.rs
+++ src/lib.rs
@@ -104,3 +104,13 @@ impl Default for Transform {
         Self::Fixed(Affine::IDENTITY)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    // CI will fail unless it can execute at least one test per workspace.
+    // Delete this dummy test once we have an actual real test.
+    #[test]
+    fn dummy_test_until_we_have_a_real_test() {
+        assert!(true);
+    }
+}
